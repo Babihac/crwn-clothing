7fc51956e5a515098b6f5431b4469388909332c5
Michal <michalvokolek89@gmail.com>
2021-01-06 01:18:00 +0100

    implemented firebase utils, included ability to store authenicated user into firestore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage.component";
 import ShopPage from "./pages/shop/shop.componnet";
 import HeaderComponent from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -16,9 +16,23 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // this.setState({ currentUser: user });
+      // console.log(user);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      } else if (!userAuth) {
+        this.setState({ currentUser: null });
+      }
     });
   }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -28,7 +28,6 @@ class Directory extends Component {
                 {
                   title: 'womens',
                   imageUrl: 'https://images.unsplash.com/photo-1540221652346-e5dd6b50f3e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80',
-                  size: 'large',
                   id: 4,
                   linkUrl: 'shop/womens',
                   size: 'large'
@@ -39,7 +38,6 @@ class Directory extends Component {
                   size: 'large',
                   id: 5,
                   linkUrl: 'shop/mens',
-                  size: 'large'
                 }],
         }
     }
